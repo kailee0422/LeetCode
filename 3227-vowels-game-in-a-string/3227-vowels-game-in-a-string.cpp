6eb45f7579166d48f24bc2668f6da31ea2bfdacc
Kevin lee <71311828+kailee0422@users.noreply.github.com>
2025-09-13 02:19:07 +0800

    Time: 16 ms (15.11%), Space: 19 MB (53.73%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,8 +1,36 @@
 class Solution {
 public:
-    bool doesAliceWin(string s) {
-        return ranges::any_of(s, [](char c) {
-            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-        });
-    }
+	bool doesAliceWin(string s) {
+		int count = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+			{
+				count++;
+			}
+		}
+		if (count == 0)
+		{
+			return false;
+		}
+		if (count % 2 == 1)
+		{
+			return true;
+		}
+		int temp = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+			{
+				temp++;
+			}
+			if (temp == count - 1)
+			{
+				if (s[i + 1] == 'a' || s[i + 1] == 'e' || s[i + 1] == 'i' || s[i + 1] == 'o' || s[i + 1] == 'u')
+				{
+					return true;
+				}
+			}
+		}return false;
+	}
 };
